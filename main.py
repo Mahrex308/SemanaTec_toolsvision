70154f67ec64b8d5b67637178aff0c50117234f7
Marino <marinohk6@gmail.com>
2021-10-29 13:00:55 -0500

    Actualizo el archivo main.py
    Se agrego los kernel de cada uno de los filtros con cada uno de sus parametros.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-from convolucion_gauss import I
 import numpy
 
 from PIL import Image
@@ -19,4 +18,16 @@ def pad_with(vector, pad_width, iaxis, kwargs):
     vector[-pad_width[1]:] = pad_value
 
 I = numpy.pad(I, 10, pad_with, padder=0)
+
+kernelTS = TopSobel(3) 
+kernelGB = GaussBlur(9,9) 
+kernelMH = MexicanHat(3,9) 
+kernelLA = laplace(9) 
+kernelLG = LaplacianGauss(9,9)
+
+imgTS = ndimage.convelve(I, kernelTS, mode='constant', cval=0.0)
+imgGB = ndimage.convelve(I, kernelGB, mode='constant', cval=0.0) 
+imgMH = ndimage.convelve(I, kernelMH, mode='constant', cval=0.0)
+imgLP = ndimage.convelve(I, kernelLA, mode='constant', cval=0.0)
+imgLG = ndimage.convelve(I, kernelLG, mode='constant', cval=0.0)
 
